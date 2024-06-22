2e71aff66cf76db7137deae6ada92b768032e4d7
Sənhan Sayilov <158567029+sanhan-debug@users.noreply.github.com>
2024-06-22 12:04:13 +0400

    Add files via upload

---
+++ 4.Let Var Const/app.js
@@ -0,0 +1,77 @@
+// Var Let Const
+
+
+/*
+------------SCOPE------------
+
+    ->global scope: her yerden tapa bilersen 
+    ->Function scope
+    ->block scope
+ */
+
+    // var deyiskenadi = 10;
+
+    // console.log(deyiskenadi);
+
+
+// var a = 5;
+// console.log(a);
+// if(true){
+
+//  var b=4;
+
+// function method1(){
+//     var a=6;  //function scope
+// if(true){
+//     var b=10 // block scope
+// }
+// while(true){
+
+// }
+// for(let i =0 ; i<=10; i++){
+
+// } 
+
+// }
+
+//var ; function scope-- ramda cox yer tutur
+//let ve const : block scope ola bilir
+
+
+// function salamlash(){
+//     var salam = "herkese salam";//function scope
+// if(true){
+//     let b = 10;
+//     console.log(b)
+// }
+//     console.log(salam);
+// }
+
+// salamlash();
+
+
+// var a = 5;
+// var a = 10;
+// console.log(a)
+
+
+//LET % CONST FERQI?
+
+//const(constant): sabit , deyishmeyen
+
+// const a = 10;
+// a= 15
+// console.log(a)
+
+// let b = 5;
+// b=4;
+// b=9;
+// console.log(b);
+
+// const user = {
+//     username:"sanhan",
+//     password: "2006"
+// }
+// user.username="sanhansayilovv"
+
+// console.log(user);
